983b3f9f23000a8d7da9850dbf8f374a94e41144
VoltaireGit <voltairearvinuy2004@gmail.com>
2025-11-06 00:20:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/stitouringapp/ui/Tour/TourFragment.java
@@ -0,0 +1,35 @@
+package com.example.stitouringapp.ui.Tour;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import com.example.stitouringapp.databinding.FragmentTourBinding;
+
+public class TourFragment extends Fragment {
+
+private FragmentTourBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        DashboardViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(DashboardViewModel.class);
+
+    binding = FragmentTourBinding.inflate(inflater, container, false);
+    View root = binding.getRoot();
+
+        final TextView textView = binding.textDashboard;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+@Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
