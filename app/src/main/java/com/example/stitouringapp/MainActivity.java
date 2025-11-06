a4ccc7ba3a5844660730c416fa1119ced07019c2
Tejadilla, JC <jctejadilla@gmail.com>
2025-11-06 17:45:03 +0800

    hihi

--- app/src/main/java/com/example/stitouringapp/MainActivity.java
+++ app/src/main/java/com/example/stitouringapp/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.stitouringapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         // Adjust system window insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
